e88d3a228f212120a4e04fd55e9692d001933b63
lamiasristy <lamiasristy@gmail.com>
2020-08-28 10:40:38 +0200

    Linters fixed.

--- main.rb
+++ main.rb
@@ -1,56 +1,21 @@
-def bubble_sort(array)
-    swap = true
-    while swap
-      i = 0
-      swap = false
-      while i < (array.length - 1)
-        if array[i] > array[i + 1]
-          array[i], array[i + 1] = array[i + 1], array[i]
-          swap = true
-        end
-        i += 1
+def bubble_sort_by(array)
+  swap = true
+  while swap
+    i = 0
+    swap = false
+    while i < (array.length - 1)
+      if yield(array[i], array[i + 1]).positive? # array[i]>array[i+1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
       end
+      i += 1
     end
-    array
-end
-  
-  puts bubble_sort([5, 3, 1, 2])
-  
-#   def bubble_sort_by(array)
-#     swap = true
-#     while swap
-#       i = 0
-#       swap = false
-#       while i < (array.length - 1)
-#         if yield(array[i], array[i + 1]) > 0 # array[i]>array[i+1]
-#           array[i], array[i + 1] = array[i + 1], array[i]
-#           swap = true
-#         end
-#         i += 1
-#       end
-#     end
-#     array
-#   end
-
-def bubble_sort_by
-    p "array is #{self}"
-    # swap = true
-    # while swap
-    #   i = 0
-    #   swap = false
-    #   while i < (array.length - 1)
-    #     if yield(array[i], array[i + 1]) > 0 # array[i]>array[i+1]
-    #       array[i], array[i + 1] = array[i + 1], array[i]
-    #       swap = true
-    #     end
-    #     i += 1
-    #   end
-    # end
-    # array
   end
-  
-#    puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
-  
-   [5,2,4,8].bubble_sort_by 
-  
-  
+  puts array
+end
+
+# puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
